f099192d026b30eef92f773c525b40d63f7a9fbd
Matthew Ngoy <matthewrulez1@gmail.com>
2025-02-26 04:22:19 -0500

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,234 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full-Time Student",
+    icon: "https://static.wixstatic.com/media/7383ad_c9a2ed5d32704d6b81504a4175fa440c~mv2.png/v1/fill/w_400,h_400,al_c,q_85,usm_1.20_1.00_0.01,enc_avif,quality_auto/CBH%20Logo.png",
+  },
+  {
+    title: "Music Enjoyer",
+    icon: "https://www.logo.wine/a/logo/Spotify/Spotify-Icon-Logo.wine.svg",
+  },
+  {
+    title: "Gamer",
+    icon: "https://opengameart.org/sites/default/files/psController.png",
+  },
+  {
+    title: "Anime Watcher",
+    icon: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b4bba41e-2420-42ec-8c4f-95844dd273dc/di50ph9-d17ac607-53e7-48ed-b2ba-d5ed3b727b8f.png/v1/fill/w_1280,h_1866/edward_elric__fullmetal_alchemist__render_v2_by_eternalashen_di50ph9-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTg2NiIsInBhdGgiOiJcL2ZcL2I0YmJhNDFlLTI0MjAtNDJlYy04YzRmLTk1ODQ0ZGQyNzNkY1wvZGk1MHBoOS1kMTdhYzYwNy01M2U3LTQ4ZWQtYjJiYS1kNWVkM2I3MjdiOGYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.XQF2Q6N93wi1jdJPURpbeH2MuacHfS3ULrQ98HPk6r4",
+  },
+];
+
+const technologies = [
+  {
+    name: "Angular",
+    icon: html,
+  },
+  {
+    name: "Python",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Expo Runner",
+    company_name: "Grillestone",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "August 2020 - April 2023",
+    points: [
+      "Maintained consistency and quality of dishes, promptly communicating issues to kitchen staff.",
+      "Exercised time management for customer orders within the food line and keen attention to detail to catch errors and inconsistencies before they reached customers.",
+      "Streamlined restaurant operations by monitoring order statuses and minimizing kitchen bottlenecks",
+    ],
+  },
+  {
+    title: "Math Tutor",
+    company_name: "Old Bridge Highschool",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Sept 2022 - June 2023",
+    points: [
+      "Position only attained by students who maintained a 4.0 GPA within the subject",
+      "Assisted with pre-algebra/algebra assignments",
+      "Met about 3x a weekwith clients from the National Honors Society throughout the year",
+    ],
+  },
+  {
+    title: "Computer Science Mentor 'Code Samurai'",
+    company_name: "Kean University",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2024 - Present",
+    points: [
+      "Position only attained through Professor recommendation",
+      "Recommended based on exceptional performance within Data Structures and Computer Organization and Architecture Course",
+      "Hosted weekly tutoring and advisement sessions related to Computer Science",
+    ],
+  },
+ 
+];
+
+const testimonials = [
+  {
+    testimonial:
+    "Your motivation, hard work, and hopefully this course will open a new way of doing research and a career.",
+    name: "Malihe Aliasgari",
+    designation: "Professor",
+    company: "Kean University",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdfpmya32VuXO6sj_UjJfOzbhlA9DQYxwGEg&s",
+  },
+  {
+    testimonial:
+      "Congratulations on being selected as an LSAMP Scholar at Kean University! This achievement reflects your dedication and potential in the STEM disciplines, and we are thrilled to support your growth in this field.",
+    name: "LSAMP",
+    designation: "National Science Foundation Organization",
+    company: "Kean University",
+    image: "https://community.okstate.edu/departments/ok-lsamp/site-files/images/logo-and-maps/nsf-logo.png",
+  },
+  {
+    testimonial:
+      "We are looking for enthusiastic and knowledgeable Samurai with a strong passion and skills for programming and problem-solving for our Code Samurai team.",
+    name: "Paolien Wang",
+    designation: "Professor",
+    company: "Kean University",
+    image: "https://media.licdn.com/dms/image/v2/C4E03AQECWEkxWzStCQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1661767919053?e=2147483647&v=beta&t=lhc7wKAnYqAD1Gr1cWNy2DWgo2hbGzimuQ8CtwJsrwg",
+  },
+];
+
+const projects = [
+  {
+    name: "Java Bank",
+    description:
+      "Java Banking System developed using Agile Development practices.",
+    tags: [
+      {
+        name: "Java",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "N/A",
+        color: "green-text-gradient",
+      },
+      {
+        name: "N/A",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "https://cdn-icons-png.flaticon.com/512/7826/7826512.png",
+    source_code_link: "https://github.com/pujandesai6/AP-CSA-Bank-Project-8A",
+  },
+  {
+    name: "W.I.P",
+    description:
+      "N/A",
+    tags: [
+      {
+        name: "",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "",
+        color: "green-text-gradient",
+      },
+      {
+        name: "",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "https://e7.pngegg.com/pngimages/993/500/png-clipart-book-books-assorted-title-books-text-school-clipart-thumbnail.png",
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "W.I.P",
+    description:
+      "N/A",
+    tags: [
+      {
+        name: "",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "",
+        color: "green-text-gradient",
+      },
+      {
+        name: "",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "https://e7.pngegg.com/pngimages/993/500/png-clipart-book-books-assorted-title-books-text-school-clipart-thumbnail.png",
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
